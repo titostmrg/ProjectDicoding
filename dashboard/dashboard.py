15eccc9a52f1b394e0812847fb7acd36457aea6f
titostmrg <91191889+titostmrg@users.noreply.github.com>
2024-03-05 14:54:17 +0700

    Add files via upload

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,12 +5,30 @@ import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='dark')
 
-st.title('Belajar Analisis Data')
+st.title('Penyewaan Sepeda')
 tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
- 
+
+all_df = pd.read_csv("./dashboard/all_data.csv")
+
 with tab1:
     st.header("Chart")
-    
+    st.container()
+    holiday_df = all_df[all_df['holiday_x'] == 1]
+    non_holiday_df = all_df[all_df['holiday_x'] == 0]
+
+    avg_holiday = holiday_df['cnt_y'].mean()
+    avg_non_holiday = non_holiday_df['cnt_y'].mean()
+
+    plt.figure(figsize=(8, 6))
+    plt.bar(['Hari Biasa', 'Hari Libur'], [avg_non_holiday, avg_holiday], color=['blue', 'orange'])
+
+    plt.title('Rata-Rata Jumlah Peminjaman Sepeda pada Hari Libur dan Hari Biasa')
+    plt.xlabel('Tipe Hari')
+    plt.ylabel('Rata-Rata Jumlah Peminjaman Sepeda')
+
+    st.pyplot(plt)
+
+
  
 with tab2:
     st.header("Tab 2")
